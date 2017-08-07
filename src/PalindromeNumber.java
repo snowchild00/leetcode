81208c510621be4a83d07fb5ffb143582f068522
snowchild00 <stigger@naver.com>
2017-08-07 23:55:18 +0900

    PalindromeNumber.java

--- src/PalindromeNumber.java
+++ src/PalindromeNumber.java
@@ -7,12 +7,23 @@ public class PalindromeNumber {
       
       for( int i = 0; i < length/2; i++ ){
         int j = length - i -1;
-        int a = x / pow(10, j);
-        int b = x % pow(10, i+1);
+        int a = x / (int) Math.pow((double)10, (double) j);
+        int b = x % (int) Math.pow((double)10, (double)(i+1));
         if( a != b )
           return false;
       }
       
       return true;  
     }
+
+
+  public static void main(String[] args){
+    PalindromeNumber obj = new PalindromeNumber();
+    int[] inputs = {10, -10, 23456, 22, 232, 234, 1};
+    for( int i = 0; i < inputs.length; i++ ){
+      System.out.println(inputs[i]);
+      System.out.println(obj.isPalindrome(inputs[i]));
+
+    }
+  }
 }
