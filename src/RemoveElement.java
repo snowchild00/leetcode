f9f389f45fecd7c9fd57363c6eedfa98759a9a19
snowchild00 <stigger@naver.com>
2017-08-08 19:58:40 +0900

    Saving

---
+++ src/RemoveElement.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int removeElement(int[] nums, int val) {
+      
+      if( nums == null || nums.length == 0 )
+        return 0;
+      
+      int len = 0;
+      for( int i = 0; i < nums.length; i++ ){
+        if( nums[i] != val ) len++;
+      }
+        
+      return len;
+    }
+}
