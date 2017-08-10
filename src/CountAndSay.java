687e21cafa5f2317513052161238e1ed1c6d39a0
snowchild00 <stigger@naver.com>
2017-08-10 16:57:47 +0900

    Saving

--- src/CountAndSay.java
+++ src/CountAndSay.java
@@ -1,7 +1,31 @@
 public class CountAndSay {
-  public String countAndSay( int n ){
+	public String countAndSay( int n ){
 
+	
+		StringBuffer sb = new StringBuffer();
+		
+		int prev = -1;
+		int repeat = 0;
+		
+		while ( n != 0 ) {
+		    int digit = n % 10;
+		    n /= 10;
+		    
+		    if ( prev == -1 ) {
+		        repeat++
+		        prev = digit;
+		    } else if ( prev != digit ) {
+		        sb.insert(0, digit)
+		        sb.insert(0, repeat)
+		        repeat = 1;
+		    } else {
+		    	repeat++;
+		    }
+		   	prev = digit;    
+		    
+		    
+		}
     
-    return "";
-  }
+		return sb.toString();
+	}
 }
